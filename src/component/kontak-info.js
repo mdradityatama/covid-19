79040121f037e42cf21dd58ce6eafccafec62f63
mdradityatama <mdimasradityatama@gmail.com>
2020-08-25 17:07:46 +0700

    penambahan kontak-info

---
+++ src/component/kontak-info.js
@@ -0,0 +1,25 @@
+class KontakInfo extends HTMLElement {
+  connectedCallback()
+  {
+    this.render();
+  }
+
+  render() 
+  {
+    this.innerHTML = 
+    `
+    <h2 class="text-center pt-5 mb-5">Kontak Info</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <a href="https://covid19.go.id/peta-sebaran">
+            <img src="https://covid19.go.id/storage/app/media/bersatu-lawan-covid-19.jpg" class="img-fluid">
+          </a>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+}
+
+customElements.define("kontak-info", KontakInfo)
